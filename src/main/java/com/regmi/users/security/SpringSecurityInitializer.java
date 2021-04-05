fb28cb805ff3e3e5bf9c87270ddcfa05349edd38
Himal-Regmi <himal.rg@gmail.com>
2021-04-05 16:05:26 +0545

    deleted a file

--- src/main/java/com/regmi/users/security/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-//package com.regmi.userApi.security;
-//
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//
-//public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-//
-//}
